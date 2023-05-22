20c7aecef4b2970964d8d06da0807fcdd425836a
Afzal <m.afzal.rhm@gmail.com>
2023-05-22 10:09:55 +0600

    working on the banner section

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,7 +1,15 @@
+import banner from "../../../assets/banner.jpg";
+
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to The Home Page!!!</h1>
+      <div className="relative">
+        <img src={banner} alt="banner" className="rounded-xl" />
+        <h1 className="absolute text-5xl left-1/2 mx-auto bottom-52">
+          Are you on the Dark Side or the Light Side?
+        </h1>
+        <h3>Choose the toys from your side!!!</h3>
+      </div>
     </div>
   );
 };
