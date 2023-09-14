d644a90153fd85b3985c8c95a1ec6079d48f51db
muhamdAfzal <afzalur.rh@gmail.com>
2023-09-14 23:47:16 +0600

    mid section complete

--- src/Pages/Home/MidSection/MidSection.jsx
+++ src/Pages/Home/MidSection/MidSection.jsx
@@ -1,9 +1,10 @@
 import babyYoda from "../../../assets/Baby Yoda Toy.png";
+import stormtrooper from "../../../assets/Stormtrooper.png";
 
 const MidSection = () => {
   return (
     <section>
-      <container className="flex gap-32 px-36 my-24 relative">
+      <container className="flex gap-32 px-36 mt-36">
         <img src={babyYoda} alt="" className="h-96" />
         <div className="mt-20">
           <h1 className="font-bold text-4xl">May The Force Be With You</h1>
@@ -12,6 +13,16 @@ const MidSection = () => {
             We troopers are ready to deliver your favorite toys to your doorstep
           </p>
         </div>
+      </container>
+      <container className="flex gap-64 px-36 mt-36">
+        <div className="mt-36">
+          <h1 className="font-bold text-4xl">Easy To Get</h1>
+          <hr className="my-6" />
+          <p className="text-2xl font-semibold">
+            Just choose your toy and order
+          </p>
+        </div>
+        <img src={stormtrooper} alt="" className="w-96" />
       </container>
     </section>
   );
